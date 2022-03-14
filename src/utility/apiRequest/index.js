ce43a6c1871ec7e9fab3bcee13437f29daa41bef
bledizogaj123 <bz47140@ubt-uni.net>
2022-03-14 18:12:48 +0100

    register,login and utility added

---
+++ src/utility/apiRequest/index.js
@@ -0,0 +1,29 @@
+import registerUser from "./user/registerUser";
+import getLibrariaList from './menaxhimiLibrarise/getLibrariaList';
+import createMenaxhimiLibrarise from './menaxhimiLibrarise/createMenaxhimiLibrarise';
+import getMenaxhimiLibrarise from './menaxhimiLibrarise/getMenaxhimiLibrarise';
+import getFilesDetails from './menaxhimiLibrarise/getFilesDetails'
+import removeFile from './menaxhimiLibrarise/removeFile';
+import deleteMenaxhimiLibrarise from './menaxhimiLibrarise/deleteMenaxhimiLibrarise'
+import createNews from "./news/createNews";
+import deleteNews from "./news/deleteNews";
+import getFilesDetailsNews from "./news/getFilesDetailsNews";
+import getNews from "./news/getNews";
+import getNewsList from "./news/getNewsList";
+import removeFileNews from "./news/removeFileNews";
+
+export default {
+    registerUser,
+    getLibrariaList,
+    createMenaxhimiLibrarise,
+    getMenaxhimiLibrarise,
+    getFilesDetails,
+    removeFile,
+    deleteMenaxhimiLibrarise,
+    createNews,
+    deleteNews,
+    getFilesDetailsNews,
+    getNews,
+    getNewsList,
+    removeFileNews,
+};
