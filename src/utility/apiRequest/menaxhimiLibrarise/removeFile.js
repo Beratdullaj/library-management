8aabbeb979fc2dc9ec205ff1ecc14692c8347f71
Beratdullaj <62146307+Beratdullaj@users.noreply.github.com>
2022-03-14 19:05:04 +0100

    remove and details done

---
+++ src/utility/apiRequest/menaxhimiLibrarise/removeFile.js
@@ -0,0 +1,14 @@
+import { getAuth } from "firebase/auth";
+import apiCaller from "../apiCaller";
+
+const removeFile = async (id, file) => {
+
+    const token = await getAuth().currentUser.getIdToken();
+
+    return apiCaller.put(`menaxhimi-librarise/${id}/removeFile/${file.name}`, {
+        headers: {authorization: `Bearer ${token}`}
+    })
+
+};
+
+export default removeFile;
