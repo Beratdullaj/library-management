e4d892c83b86ecf8722adc3790141f6ea786f90e
bledizogaj123 <bz47140@ubt-uni.net>
2022-03-14 18:17:24 +0100

    authentication done

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+      <Header/>    
+      <router-view />
+
+      <Footer/>
+  
+   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from '@/components/layout/Header'
+import Footer from '@/components/layout/Footer'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Footer,
   }
 }
 </script>
+
 
 <style>
 #app {
@@ -23,6 +27,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
