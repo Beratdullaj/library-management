0470789c921243f06897cca4033acb0df0650a41
bledizogaj123 <bz47140@ubt-uni.net>
2022-03-14 17:47:40 +0100

    First commit

---
+++ menaxhimi-librarise-api/server.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import usersRouter from './api/routes/usersRoute';
+import getAuthToken from './api/middlewares/getAuthToken';
+import cors from 'cors';
+import helmet from 'helmet';
+import menaxhimiLibrariseRoute from './api/routes/menaxhimiLibrariseRoute';
+import mongoose from 'mongoose';
+import fileupload from"express-fileupload";
+import newsRoute from './api/routes/newsRoute';
+
+mongoose.connect("mongodb://localhost:27017/menaxhimi-librarise-db").then( () => {
+  console.log('Connected to mongodb, port 27017!');
+
+  
+const port = process.env.PORT || 4000;
+
+
+const app = express();
+
+app.use(
+  cors({
+  origin:"*"
+}));
+
+// static/:filename
+app.use('/static', express.static(`${__dirname}/public/files`));
+
+app.use(helmet());
+
+app.use(fileupload());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.use(getAuthToken);
+
+app.use('/users', usersRouter );
+
+app.use('/menaxhimi-librarise', menaxhimiLibrariseRoute);
+app.use('/news', newsRoute);
+
+app.listen(port, () => {
+    console.log(`Menaxhimi i librarise API running on port ${port}`);
+})
+} )
+
